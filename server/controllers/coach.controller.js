34eeae7d0f516b3dd4d86dc0b4646698ca6d6616
Ryan Beatty <rbeatty1196@gmail.com>
2020-12-10 14:30:55 -0600

    Added coach controller

---
+++ server/controllers/coach.controller.js
@@ -0,0 +1,17 @@
+const db = require("../helpers/db");
+const User = db.User;
+const Team = db.Team;
+
+const makeTeam = (req, res) => {
+    let newTeam = new Team({
+        teamName: req.params[teamname],
+        coach: req.params[coach], 
+        description: req.params[description],
+    });
+
+    newTeam.save((err, data) => {
+        if(err) return console.error(err);
+        console.log(data);
+    });
+};
+module.exports = makeTeam;
