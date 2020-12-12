d2a30629f5762fc6fa86675681e31e7270f7db59
Unknowncall <zharvey3131@gmail.com>
2020-12-12 03:19:06 -0600

    Removed unused vars

--- src/components/dashboard/SupportServiceCard.js
+++ src/components/dashboard/SupportServiceCard.js
@@ -1,47 +1,69 @@
-import React from 'react';
-import { Card, CardContent, CardMedia, CardActions, Collapse, Typography, IconButton, makeStyles, Grid, Badge } from '@material-ui/core';
-import { ExpandMore } from '@material-ui/icons';
-import clsx from 'clsx';
+import React from "react";
+import {
+  Card,
+  CardContent,
+  CardMedia,
+  CardActions,
+  Collapse,
+  Typography,
+  IconButton,
+  makeStyles,
+  Grid,
+  Badge,
+} from "@material-ui/core";
+import { ExpandMore } from "@material-ui/icons";
+import clsx from "clsx";
 
-const cardStyles = makeStyles(theme => ({
+const cardStyles = makeStyles((theme) => ({
   root: {
     maxWidth: 345,
   },
   media: {
     height: 0,
-    paddingTop: '56.25%', // 16:9
+    paddingTop: "56.25%", // 16:9
   },
   expand: {
-    transform: 'rotate(0deg)',
-    marginLeft: 'auto',
-    transition: theme.transitions.create('transform', {
+    transform: "rotate(0deg)",
+    marginLeft: "auto",
+    transition: theme.transitions.create("transform", {
       duration: theme.transitions.duration.shortest,
     }),
   },
   expandOpen: {
-    transform: 'rotate(180deg)',
+    transform: "rotate(180deg)",
   },
 }));
 
 export default function SupportServiceCard(props) {
   const classes = cardStyles();
-  const { image, header, link, description, expandedText, color, notifications } = props;
+  const {
+    image,
+    header,
+    description,
+    expandedText,
+    color,
+    notifications,
+  } = props;
   const [expanded, setExpanded] = React.useState(false);
 
   const handleExpandClick = () => {
     setExpanded(!expanded);
-  }
+  };
 
   return (
     <Grid item lg={3} sm={6} xl={3} xs={12}>
-      {notifications
-        ? <Badge Badge color="secondary" overlap="rectangle" badgeContent={notifications}>
-          <Card className={classes.root} style={{ backgroundColor: color, color: '#fff' }}>
-            <CardMedia
-              className={classes.media}
-              image={image}
-              title={header}
-            />
+      {notifications ? (
+        <Badge
+          Badge
+          color="secondary"
+          overlap="rectangle"
+          badgeContent={notifications}
+        >
+          <Card
+            className={classes.root}
+            style={{ backgroundColor: color, color: "#fff" }}
+          >
+            <CardMedia className={classes.media} image={image} title={header} />
             <CardContent>
               <Typography gutterBottom variant="h5" component="h2">
                 {header}
@@ -64,19 +86,17 @@ export default function SupportServiceCard(props) {
             </CardActions>
             <Collapse in={expanded} timeout="auto" unmountOnExit>
               <CardContent>
-                <Typography>
-                  {expandedText}
-                </Typography>
+                <Typography>{expandedText}</Typography>
               </CardContent>
             </Collapse>
           </Card>
         </Badge>
-        : <Card className={classes.root} style={{ backgroundColor: color, color: '#fff' }}>
-          <CardMedia
-            className={classes.media}
-            image={image}
-            title={header}
-          />
+      ) : (
+        <Card
+          className={classes.root}
+          style={{ backgroundColor: color, color: "#fff" }}
+        >
+          <CardMedia className={classes.media} image={image} title={header} />
           <CardContent>
             <Typography gutterBottom variant="h5" component="h2">
               {header}
@@ -99,13 +119,11 @@ export default function SupportServiceCard(props) {
           </CardActions>
           <Collapse in={expanded} timeout="auto" unmountOnExit>
             <CardContent>
-              <Typography>
-                {expandedText}
-              </Typography>
+              <Typography>{expandedText}</Typography>
             </CardContent>
           </Collapse>
         </Card>
-      }
+      )}
     </Grid>
-  )
+  );
 }
