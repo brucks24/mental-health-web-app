e0449093cd1f836140cecf71a36ac996e864b016
uiyotp <solomonpaprocki@gmail.com>
2021-05-06 09:28:34 -0500

    Cleaned up panic button.  Added comments for panic and sendMessage.

--- src/components/common/drawer/components/PanicButton.js
+++ src/components/common/drawer/components/PanicButton.js
@@ -7,7 +7,8 @@ import {
   DialogContent,
   DialogContentText,
   DialogTitle,
-  FormControlLabel
+  FormControlLabel,
+  FormGroup
 } from '@material-ui/core';
 import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
 import useStyles from './styles';
@@ -15,6 +16,7 @@ import WarningIcon from '@material-ui/icons/WarningRounded';
 import { purple, red } from '@material-ui/core/colors';
 import { useDispatch } from 'react-redux';
 import { panicButton } from '../../../../redux/actions/userActions';
+import { LabelSharp } from '@material-ui/icons';
 
 const theme = createMuiTheme({
   palette: {
@@ -22,51 +24,73 @@ const theme = createMuiTheme({
   }
 });
 
-const buttonTheme = createMuiTheme({ palette: { primary: purple, secondary: red } });
+const buttonTheme = createMuiTheme({ palette: { primary: red} });
 
 function PanicButton(props) {
   const classes = useStyles();
   const [open, setOpen] = React.useState(false);
   const [state, setState] = React.useState({
-    button1: 'primary',
-    button2: 'primary',
-    button3: 'primary',
-    button4: 'primary'
+    suicide: false,
+    homicide: false,
+    sexualAssault: false,
+    bullying: false
   });
   const dispatch = useDispatch();
+  const { suicide, homicide, sexualAssault, bullying } = state;
+  const labels = {
+    suicide: "Concerns of suicide",
+    homicide: "Concerns of homicide",
+    sexualAssault: "Sexual harassment and/or assault",
+    bullying: "Concerns about bullying and/or hazing"
+  }
+  let reasons = [];
 
-  let buttonsClicked = [];
+  //Resets all checkboxes to unchecked, called after modal is closed or submitted
+  const resetState = () => {
+    setState({
+      suicide: false,
+      homicide: false,
+      sexualAssault: false,
+      bullying: false
+    });
+  }
 
+  //Opens the modal
   const handleClickOpen = () => {
     setOpen(true);
   }
 
+  //Resets the checkboxes and closes the modal
   const handleClose = () => {
+    resetState();
     setOpen(false);
   }
 
+  //Sends reasons to userActions.js, resets the checkboxes, closes the modal
   const handleConfirm = () => {
-    if (state.button1 === 'secondary')
-      buttonsClicked.push('Concerns of suicide');
-    if (state.button2 === 'secondary')
-      buttonsClicked.push('Concerns of homicide');
-    if (state.button3 === 'secondary')
-      buttonsClicked.push('Sexual harassment and/or assult');
-    if (state.button4 === 'secondary')
-      buttonsClicked.push('Concerns about bullying and/or hazing');
-
-    dispatch(panicButton(buttonsClicked));
+    if(suicide){
+      reasons.push(labels.suicide);
+    }
+    if(homicide){
+      reasons.push(labels.homicide);
+    }
+    if(sexualAssault){
+      reasons.push(labels.sexualAssault);
+    }
+    if(bullying){
+      reasons.push(labels.bullying);
+    }
+    dispatch(panicButton(reasons));
+    resetState();
     setOpen(false);
   }
 
-  const handleChange = name => {
-    if (state[name] === 'secondary') {
-      setState({ ...state, [name]: 'primary' });
-    } else {
-      setState({ ...state, [name]: 'secondary' });
-    }
-  }
+  //Handles the checking and unchecking of the checkboxes
+  const handleChange = (event) => {
+    setState({ ...state, [event.target.name]: event.target.checked });
+  };
 
+  //Displays the modal
   return (
     <div>
       <ThemeProvider theme={theme}>
@@ -86,45 +110,62 @@ function PanicButton(props) {
         aria-labelledby="alert-dialog-title"
         aria-describedby="alert-dialog-description"
       >
-        <DialogTitle id="alert-dialog-title">{"If you are in a life threatening situation, or this is a medical emergency, please, close this app and dial 911 immediately."}</DialogTitle>
         <DialogContent>
-          <DialogContentText id="alert-dialog-description">
-            Select the boxes that apply to you, then select "I Understand":
-            <ThemeProvider theme={buttonTheme}>
+        <ThemeProvider theme={buttonTheme, theme}>
+          <DialogContentText id="alert-dialog-description" color="black">
+          If you are in a life threatening situation, or this is a medical emergency, please, close this app and dial 911 immediately.  Select the boxes that apply to you, then select "I Understand":
+            <FormGroup>
               <FormControlLabel
                 control={
                   <Checkbox
                   className={classes.reasonButton}
-                  size="medium"
-                  color={state.button1}
-                  onClick={() => handleChange('button1')}
+                  onChange={handleChange}
+                  checked={suicide}
+                  name="suicide"
+                  color="primary"
                 ></Checkbox>
                 }
-                label="Concerns of suicide"
+                label={labels.suicide}
               />
-              <Button
-                className={classes.reasonButton}
-                variant="contained"
-                size="large"
-                color={state.button2}
-                onClick={() => handleChange('button2')}
-              >Concerns of homicide</Button>
-              <Button
-                className={classes.reasonButton}
-                variant="contained"
-                size="large"
-                color={state.button3}
-                onClick={() => handleChange('button3')}
-              >Sexual harassment and/or assault</Button>
-              <Button
-                className={classes.reasonButton}
-                variant="contained"
-                size="large"
-                color={state.button4}
-                onClick={() => handleChange('button4')}
-              >Concerns about bullying and/or hazing</Button>
-            </ThemeProvider>
+              <FormControlLabel
+                control={
+                  <Checkbox
+                  className={classes.reasonButton}
+                  onChange={handleChange}
+                  checked={homicide}
+                  name="homicide"
+                  color="primary"
+                ></Checkbox>
+                }
+                label={labels.homicide}
+              />
+              <FormControlLabel
+                control={
+                  <Checkbox
+                  className={classes.reasonButton}
+                  onChange={handleChange}
+                  checked={sexualAssault}
+                  name="sexualAssault"
+                  color="primary"
+                ></Checkbox>
+                }
+                label={labels.sexualAssault}
+              />
+              <FormControlLabel
+                control={
+                  <Checkbox
+                  className={classes.reasonButton}
+                  onChange={handleChange}
+                  checked={bullying}
+                  name="bullying"
+                  color="primary"
+                ></Checkbox>
+                }
+                label={labels.bullying}
+              />
+            </FormGroup>
           </DialogContentText>
+          </ThemeProvider>
         </DialogContent>
         <DialogActions>
           <Button onClick={handleClose} color="primary">Exit</Button>
